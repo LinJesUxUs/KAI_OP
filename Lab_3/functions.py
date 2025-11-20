aff0efb5887011a8910c427db0b97294fde0e5f7
radikru96 <radikru96@gmail.com>
2025-11-20 14:31:52 +0300

    functions added

---
+++ Lab_3/functions.py
@@ -0,0 +1,37 @@
+from typing import Final
+import random
+
+def randomList():
+    INT_MAX: Final = int(2**8/2-1)
+    INT_MIN: Final = int(-2**8/2)
+    
+    n = int(0)
+    A = []
+    
+    # Обработка ввода колличества случайных переменных
+    def customRand(error: bool = False):
+        global n
+        if error:
+            print("Колличество будет определено случайно!")
+            n = random.randint(1,INT_MAX)
+        else: return random.randint(INT_MIN,INT_MAX)
+    
+    try:
+        n = int(input("Введите колличество случайных переменных:\n"))
+        if n == 0:
+            raise
+    except ValueError:
+        print("Введено не целочисленное число")
+        customRand(True)
+    except:
+        print("Введён 'n' равный 0")
+        customRand(True)
+    
+    if n < 0 :
+        print("Введено отрицительное число. Беру его по модулю")
+        n = abs(n)
+    
+    # Заполнение случайными переменными
+    for i in range(n):
+        A.append(customRand())
+    return A
