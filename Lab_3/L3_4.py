778266933028fa4b16fb88bf066ecc0f6b924daf
radikru96 <radikru96@gmail.com>
2025-11-20 22:59:29 +0300

    Lab3.4 success

--- Lab_3/L3_4.py
+++ Lab_3/L3_4.py
@@ -1,18 +1,26 @@
 
 print( "Лаба-3. Задание-4" )
 
-n=0
+n = 0
 
 while(True):
 	try: n = int(input(
-		"Введите предельное целое число последовательности."))
+		"Введите предельное целое число последовательности.\n"))
 	except: print("Введено не целое число!")
 	else: break
 
-st = {i for i in range(2,n)}
-lst = []
+# множество кратное 7
+x = 7
+st = {i for i in range(x,n,x)}
 
-for i in range(2,n): #нужно i домножать на 2 при итерации
+p = [i for i in range(2, n)]
 
+for i in p:
+	# итерация от 'p' в квадрате, так как 'p' простое число,
+	# следующее кратное 'p' является квадратом 'p'
+	for j in range(i**2, p[-1], i):
+		try: p.remove(j)
+		except: continue
 
-print(*st)
+print("Числа кратные",x,":",*st)
+print("Простые числа:",*p)
