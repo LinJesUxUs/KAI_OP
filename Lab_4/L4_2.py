5da1b65c915605151f815d9568baf2a4f7a51e0a
radikru96 <radikru96@gmail.com>
2025-12-17 17:09:56 +0300

    recursion faster than buffering

--- Lab_4/L4_2.py
+++ Lab_4/L4_2.py
@@ -3,33 +3,60 @@
 # a(n)=a(n div 2)+a(n div 3), n>1;
 
 from functions import getUIntWithMsg
-
+import time
 print("Лаба-4. Задание-4.5")
 
 def aRecurs(n):
 	if n == 0:
 		return 1
-	return a( n // 2 ) + a( n // 3 )
+	return aRecurs( n // 2 ) + aRecurs( n // 3 )
 
 def a(n):
 	buf = {}
 	buf[0] = 1
+	# if n in buf: return buf[n]
 	for i in range(1,n+1):
+		# if i not in buf:
 		buf[i] = buf[i//2] + buf[i//3]
 	return buf[n]
 
-n = getUIntWithMsg("Введите длинну последовательности:","Введите целое целое!")
-print("")
+# buf = {}
+# buf[0] = 1
+# def a(n):
+# 	global buf
+# 	if n in buf: return buf[n]
+# 	for i in range(1,n+1):
+# 		if i not in buf:
+# 			buf[i] = buf[i//2] + buf[i//3]
+# 	return buf[n]
 
+# n = getUIntWithMsg("Введите длинну последовательности:","Введите целое целое!")
+# print("")
+
+# for i in range(n):
+# 	print(aRecurs(i), end=" ")
+
+# print("")
+
+# for i in range(n):
+# 	print(a(i), end=" ")
+
+# print("")
+
+n = 10000
+start = time.perf_counter()
 for i in range(n):
-	print(aRecurs(i), end=" ")
-
-print("")
-
+	aRecurs(i)
+print(f"aRecurs({n}): {(time.perf_counter()-start):.6f} seconds")
+start = time.perf_counter()
 for i in range(n):
-	print(a(i), end=" ")
-
-print("")
-
-# print(aRecurs(1000000))
-# print(a(1000000))
+	a(i)
+print(f"a({n}): {(time.perf_counter()-start):.6f} seconds")
+start = time.perf_counter()
+for i in range(n):
+	aRecurs(i)
+print(f"aRecurs({n+1}): {(time.perf_counter()-start):.6f} seconds")
+start = time.perf_counter()
+for i in range(n):
+	a(i)
+print(f"a({n+1}): {(time.perf_counter()-start):.6f} seconds")
