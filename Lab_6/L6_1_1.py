772db86b719d0650e75da30a820ae0a1053310dd
radikru96 <radikru96@gmail.com>
2026-01-18 22:47:38 +0300

    L6.1.1 success

---
+++ Lab_6/L6_1_1.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+c = Canvas(width=460,height=460,bg='grey80')
+c.pack()
+
+oval = c.create_oval(30,10,130,80)
+rect = c.create_rectangle(180,10,280,80)
+trian = c.create_polygon(330,80,380,10,430,80, fill='grey80',outline="black")
+
+c.move(rect,0,150)
+c.itemconfig(trian,outline="red",width=3)
+c.coords(oval,300,200,450,450)
+
+def mooove(event):
+    c.move(rect,0,2)
+c.bind('<Button-1>',mooove)
+
+oval2 = c.create_oval(30,10,130,80,tag="group1")
+c.create_line(10,100,450,100,tag="group1")
+
+def color(event):
+    if ( c.itemcget(oval2, "fill") == "red" ):
+        c.itemconfig('group1',fill="black",width=1)
+        c.itemconfig(oval2,fill="")
+    else: c.itemconfig('group1',fill="red",width=3)
+c.bind('<Button-3>',color)
+
+def clean(event):
+    c.delete(oval, trian)
+c.bind('<Button-2>',clean)
+
+mainloop()
