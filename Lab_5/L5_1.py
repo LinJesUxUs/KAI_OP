c45b1ace168ab6963aa5e2cdb25d542eee1f1f39
radikru96 <radikru96@gmail.com>
2026-02-09 20:48:02 +0300

    L5_1 add comments

--- Lab_5/L5_1.py
+++ Lab_5/L5_1.py
@@ -8,12 +8,13 @@
 print("Лаба-5. Задание-5.1")
 
 def getRecursCloseBraceIndex (text: str, begin_char, end_char):
+	# Поиск закрывающего символа, с учётом вложений
 	length = -1
 	count = 0
 	for i in text:
 		length += 1
-		if i == begin_char: count += 1
-		elif i == end_char: count -= 1
+		if i == begin_char: count += 1 # найден открывающий символ, +1 вложенности
+		elif i == end_char: count -= 1 # найден закрывающий символ, -1 вложенности
 		if count == 0: break
 	return length
 
@@ -23,8 +24,8 @@ begin_char = '{'
 end_char = '}'
 
 while(True):
-	begin = text.find(begin_char)
-	if begin < 0: break
+	begin = text.find(begin_char) # индекс первого открывающего символа
+	if begin < 0: break # больше нет открывающих символов
 	end = begin + getRecursCloseBraceIndex(text[begin:], begin_char, end_char)
 	text = text[:begin] + text[end+1:]
 
