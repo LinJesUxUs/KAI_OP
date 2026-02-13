a1ac111e8d2ab2433ac686029044c64535539e32
radikru96 <radikru96@gmail.com>
2026-02-13 18:03:40 +0300

    Lab7 in dev: udpate

--- Lab_7/L7.py
+++ Lab_7/L7.py
@@ -7,20 +7,29 @@ from datetime import datetime
 
 class TrashWords:
 	def __init__(self):
-		self.strings_list = []
-		self.lock = threading.Lock()
-		self.running = True
+		self.__strings_list = []
+		self.__lock = threading.Lock()
+		self.__running = True
 		self.__screen_width = shutil.get_terminal_size().columns
-		self.chars = string.ascii_letters + string.digits + ' ' + string.punctuation
+		self.__chars = string.ascii_letters + string.digits + ' ' + string.punctuation
 
 	def get_random_length(self):
 		return random.randint( 1, self.__screen_width )
 
 	def get_random_char(self):
-		return random.choice( self.chars )
+		return random.choice( self.__chars )
 
 	def get_random_string(self):
 		return ''.join( self.get_random_char() for _ in range( self.get_random_length() ) )
 
+	def main_thread(self):
+		self.__strings_list.append( self.get_random_string() )
+
+	def get_string_list(self):
+		return self.__strings_list
+
 obj = TrashWords()
-print( obj.get_random_string() )
+obj.main_thread()
+print( *obj.get_string_list() )
+obj.main_thread()
+print( *obj.get_string_list(), sep='\n' )
